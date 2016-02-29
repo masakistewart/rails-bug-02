67cd430a93fa474dd4447acafa7ef873df33df67
Cairo Stewart <stewartcairo@gmail.com>
2016-02-29 15:40:25 -0800

    bug fixed

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 require 'capybara/rails'
 
 feature 'Auth' do
-
   scenario 'Users can login and out' do
     create_user email: "user@example.com"
 
@@ -16,4 +15,18 @@ feature 'Auth' do
     expect(page).to have_content("user@example.com")
   end
 
+  scenario 'Once a user is created they should be logged in' do
+    create_user email: "user@example.com"
+
+    visit root_path
+    within(".auth") { click_on "Register" }
+    expect(page).to have_content("Register")
+
+    fill_in "Email", with: "user@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Confirm", with: "password"
+
+    within('.registration-form') { click_on "Register"}
+    expect(current_path).to eql(registrations_path)
+  end
 end
